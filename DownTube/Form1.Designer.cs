600e8aa129fbb67c2a0f85617f099862e1dda68b
Roberto David Ramirez <davidrodar@gmail.com>
2019-03-27 17:05:26 -0600

    First Commit
    Basic Functionality Works

---
+++ DownTube/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace DownTube
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtRes = new System.Windows.Forms.TextBox();
+            this.marqueeProgressBarControl1 = new DevExpress.XtraEditors.MarqueeProgressBarControl();
+            this.alertControl1 = new DevExpress.XtraBars.Alerter.AlertControl(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.marqueeProgressBarControl1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(364, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(382, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Check";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(382, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 46);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Download";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nombre: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Resolución:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(81, 40);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(295, 20);
+            this.txtName.TabIndex = 5;
+            // 
+            // txtRes
+            // 
+            this.txtRes.Location = new System.Drawing.Point(81, 66);
+            this.txtRes.Name = "txtRes";
+            this.txtRes.Size = new System.Drawing.Size(295, 20);
+            this.txtRes.TabIndex = 6;
+            // 
+            // marqueeProgressBarControl1
+            // 
+            this.marqueeProgressBarControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.marqueeProgressBarControl1.Location = new System.Drawing.Point(0, 124);
+            this.marqueeProgressBarControl1.Name = "marqueeProgressBarControl1";
+            this.marqueeProgressBarControl1.Properties.LookAndFeel.SkinName = "Office 2013 Dark Gray";
+            this.marqueeProgressBarControl1.Properties.LookAndFeel.UseDefaultLookAndFeel = false;
+            this.marqueeProgressBarControl1.Properties.MarqueeAnimationSpeed = 50;
+            this.marqueeProgressBarControl1.Properties.ProgressAnimationMode = DevExpress.Utils.Drawing.ProgressAnimationMode.PingPong;
+            this.marqueeProgressBarControl1.Size = new System.Drawing.Size(471, 11);
+            this.marqueeProgressBarControl1.TabIndex = 8;
+            this.marqueeProgressBarControl1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(471, 135);
+            this.Controls.Add(this.marqueeProgressBarControl1);
+            this.Controls.Add(this.txtRes);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "DownTube";
+            ((System.ComponentModel.ISupportInitialize)(this.marqueeProgressBarControl1.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtRes;
+        private DevExpress.XtraEditors.MarqueeProgressBarControl marqueeProgressBarControl1;
+        private DevExpress.XtraBars.Alerter.AlertControl alertControl1;
+    }
+}
+
---
+++ DownTube/Form1.cs
@@ -0,0 +1,56 @@
+﻿
+
+using System.Reflection;
+using System.Threading.Tasks;
+using DevExpress.XtraBars.Alerter;
+
+namespace DownTube
+{
+    using System;
+    using System.Windows.Forms;
+    using System.IO;
+
+    using VideoLibrary;
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            var service = Client.For(YouTube.Default);
+            var video = service.GetVideo(textBox1.Text);
+            txtName.Text = video.Title;
+            txtRes.Text = video.Resolution.ToString();
+            
+        }
+
+        private async void DownloadVideo(string uri)
+        {
+           
+            marqueeProgressBarControl1.Visible = true;
+            
+            var service = Client.For(YouTube.Default);
+            var video = service.GetVideo(uri);
+
+            string path = Path.Combine("D:\\Videos", video.FullName);
+            byte[] videoBytes = await video.GetBytesAsync();
+            File.WriteAllBytes(path, videoBytes);
+            Cursor.Current = Cursors.Default;
+            //toastNotificationsManager1.ShowNotification("a43f4cca-6605-4096-9f89-2801ec1fa96f");
+            AlertInfo info = new AlertInfo("Video Descargado", $"El video \"{txtName.Text}\" ha sido descargado ");
+            alertControl1.Show(this,info);
+            marqueeProgressBarControl1.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DownloadVideo(textBox1.Text);
+        }
+
+       
+    }
+}
